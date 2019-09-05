b846602de39be3a959b19e052629be6fdad88ca3
Ryo Nakamura <upa@haeena.net>
2019-09-05 17:03:10 +0900

    nettlp: shorten output w/o verbose

--- print-nettlp.c
+++ print-nettlp.c
@@ -47,11 +47,11 @@ void nettlp_print_mr(netdissect_options *ndo, const struct tlp_mr_hdr *tlpm,
 	a = (uint32_t *)(tlpm + 1);
 	addr = ntohl(*a);
 	if (tlp_is_3dw(tlpm->tlp.fmt_type)) {
-		ND_PRINT("Addr 0x%08lx", addr);
+		ND_PRINT("Addr 0x%08llx", addr);
 	} else if (tlp_is_4dw(tlpm->tlp.fmt_type)) {
 		a++;
 		addr = (addr << 32) | ntohl(*a);
-		ND_PRINT("Addr 0x%016lx", addr);
+		ND_PRINT("Addr 0x%016llx", addr);
 	}
 
 	return;
@@ -77,7 +77,7 @@ void nettlp_print_cpl(netdissect_options *ndo, const struct tlp_cpl_hdr *tlpc,
 	else if (tlp_cpl_status(tlpc->stcnt) == TLP_CPL_STATUS_CA)
 		ND_PRINT("completer abort, ");
 
-	ND_PRINT("byte count %d, ", tlp_cpl_bcnt(tlpc->stcnt));
+	ND_PRINT("bc %d, ", tlp_cpl_bcnt(tlpc->stcnt));
 	ND_PRINT("requester %02x:%02x, ", tlp_id_to_bus(tlpc->requester),
 		 tlp_id_to_device(tlpc->requester));
 	ND_PRINT("tag 0x%02x, ", tlpc->tag);
@@ -106,25 +106,30 @@ nettlp_print_tlp(netdissect_options *ndo, const struct tlp_hdr *tlp,
 		return;
 	}
 	     
-	if (tlp_is_3dw(tlp->fmt_type))
-		ND_PRINT("3DW, ");
-	else if (tlp_is_4dw(tlp->fmt_type))
-		ND_PRINT("4DW, ");
-	else {
-		ND_PRINT("Invalid DW 0x%x", tlp->fmt_type); 
-		return;
+	if (ndo->ndo_vflag) {
+
+		if (tlp_is_3dw(tlp->fmt_type))
+			ND_PRINT("3DW, ");
+		else if (tlp_is_4dw(tlp->fmt_type))
+			ND_PRINT("4DW, ");
+		else {
+			ND_PRINT("Invalid DW 0x%x", tlp->fmt_type); 
+			return;
+		}
+
+		if (tlp_is_w_data(tlp->fmt_type))
+			ND_PRINT("WD, ");
+
+		ND_PRINT("tc %x, ", tlp_tclass(tlp->tclass));
+		ND_PRINT("flags [%s], ",
+			 bittok2str_nosep(tlp_flags, "none",
+					  tlp_flag(tlp->falen)));
+		ND_PRINT("attrs [%s], ",
+			 bittok2str_nosep(tlp_attrs, "none",
+					  tlp_attr(tlp->falen)));
 	}
 
-	if (tlp_is_w_data(tlp->fmt_type))
-		ND_PRINT("WD, ");
-
-	ND_PRINT("tc %x, ", tlp_tclass(tlp->tclass));
-	ND_PRINT("flags [%s], ", bittok2str_nosep(tlp_flags, "none",
-						  tlp_flag(tlp->falen)));
-	ND_PRINT("attrs [%s], ", bittok2str_nosep(tlp_attrs, "none",
-						  tlp_attr(tlp->falen)));
 	ND_PRINT("len %d, ", tlp_length(tlp->falen));
-
 
 	if (tlp_is_mrd(tlp->fmt_type) || tlp_is_mwr(tlp->fmt_type))
 		nettlp_print_mr(ndo, (const struct tlp_mr_hdr *)tlp, length);
